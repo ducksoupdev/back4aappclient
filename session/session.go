1e2a1ee0dc106f9571eae641ef984d3b4859513d
Matt Levy <ducksoupdev@gmail.com>
2023-12-12 08:02:45 +0000

    feat: add session

--- session/session.go
+++ session/session.go
@@ -1,7 +1,11 @@
 package session
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 )
@@ -38,19 +42,116 @@ func (s *Session) initialize() {
 	}
 }
 
-func (s *Session) login(username string, password string) {
-	r, _ := s.client.Get(fmt.Sprintf("%s", s.baseUrl))
-	r.Header.Add(contentTypeHeader, contentTypeValue)
-	r.Header.Add(applicationIdHeader, s.applicationId)
-	r.Header.Add(restApiKeyHeader, s.restApiKey)
-	r.Header.Add(revocableHeader, "1")
-	// TODO: add JSON encoded query params for username and password
+func (s *Session) login(username string, password string) (string, error) {
+	// Define the parameters
+	params := url.Values{}
+	usernameB, _ := json.Marshal(username)
+	passwordB, _ := json.Marshal(password)
+	params.Add("username", string(usernameB))
+	params.Add("password", string(passwordB))
+
+	// Create the URL with the parameters
+	loginUrl, _ := url.Parse("/login")
+	joinedUrl := s.baseUrl.ResolveReference(loginUrl)
+	joinedUrl.RawQuery = params.Encode()
+
+	// create the request
+	req, _ := http.NewRequest("GET", joinedUrl.String(), nil)
+	req.Header.Add(contentTypeHeader, contentTypeValue)
+	req.Header.Add(applicationIdHeader, s.applicationId)
+	req.Header.Add(restApiKeyHeader, s.restApiKey)
+	req.Header.Add(revocableHeader, "1")
+
+	// Make the request
+	resp, err := s.client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}(resp.Body)
+
+	// Parse the response
+	var result map[string]interface{}
+	err = json.NewDecoder(resp.Body).Decode(&result)
+	if err != nil {
+		return "", err
+	}
+	s.sessionToken = result["sessionToken"].(string)
+	return s.sessionToken, nil
 }
 
-func (s *Session) createUser() {
+func (s *Session) createUser(data map[string]interface{}) (string, error) {
+	usersUrl, _ := url.Parse("/users")
+	createUserUrl := s.baseUrl.ResolveReference(usersUrl)
 
+	// create the body
+	marshalled, _ := json.Marshal(data)
+
+	// create the request
+	req, _ := http.NewRequest("POST", createUserUrl.String(), bytes.NewReader(marshalled))
+	req.Header.Add(contentTypeHeader, contentTypeValue)
+	req.Header.Add(applicationIdHeader, s.applicationId)
+	req.Header.Add(restApiKeyHeader, s.restApiKey)
+	req.Header.Add(revocableHeader, "1")
+
+	// Make the request
+	resp, err := s.client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}(resp.Body)
+
+	// Parse the response
+	var result map[string]interface{}
+	err = json.NewDecoder(resp.Body).Decode(&result)
+	if err != nil {
+		return "", err
+	}
+	s.sessionToken = result["sessionToken"].(string)
+	return s.sessionToken, nil
 }
 
-func (s *Session) passwordReset(email string) {
+func (s *Session) passwordReset(email string) error {
+	resetUrl, _ := url.Parse("/requestPasswordReset")
+	requestPasswordResetUrl := s.baseUrl.ResolveReference(resetUrl)
 
+	// create the body
+	var jsonBody = []byte(fmt.Sprintf(`{"email":"%s"}`, email))
+
+	// create the request
+	req, _ := http.NewRequest("POST", requestPasswordResetUrl.String(), bytes.NewBuffer(jsonBody))
+	req.Header.Add(contentTypeHeader, contentTypeValue)
+	req.Header.Add(applicationIdHeader, s.applicationId)
+	req.Header.Add(restApiKeyHeader, s.restApiKey)
+
+	// Make the request
+	resp, err := s.client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}(resp.Body)
+
+	// Parse the response
+	if resp.StatusCode != http.StatusOK {
+		return errors.New("request password reset failed")
+	}
+
+	return nil
 }
--- session/session_test.go
+++ session/session_test.go
@@ -1,1 +1,90 @@
 package session
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestNewSession(t *testing.T) {
+	s := NewSession(nil)
+	if s.client == nil {
+		t.Error("Expected client to be initialized")
+	}
+	if s.baseUrl == nil {
+		t.Error("Expected baseUrl to be initialized")
+	}
+}
+
+func TestInitialize(t *testing.T) {
+	s := NewSession(nil)
+	s.initialize()
+	if s.client == nil {
+		t.Error("Expected client to be initialized")
+	}
+	if s.baseUrl == nil {
+		t.Error("Expected baseUrl to be initialized")
+	}
+	if s.baseUrl.String() != "https://parseapi.back4app.com" {
+		t.Error("Expected baseUrl to be initialized with back4appBaseUrl")
+	}
+}
+
+func TestLogin(t *testing.T) {
+	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"sessionToken":"sessionToken"}`))
+	}))
+	defer svr.Close()
+	s := NewSession(nil)
+	s.baseUrl, _ = s.baseUrl.Parse(svr.URL)
+	s.applicationId = "applicationId"
+	s.restApiKey = "restApiKey"
+	s.initialize()
+	sessionToken, _ := s.login("username", "password")
+	if sessionToken == "" {
+		t.Error("Expected sessionToken to be initialized")
+	}
+	if s.sessionToken == "" {
+		t.Error("Expected sessionToken to be initialized")
+	}
+}
+
+func TestCreateUser(t *testing.T) {
+	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte(`{"sessionToken":"sessionToken"}`))
+	}))
+	defer svr.Close()
+	s := NewSession(nil)
+	s.baseUrl, _ = s.baseUrl.Parse(svr.URL)
+	s.applicationId = "applicationId"
+	s.restApiKey = "restApiKey"
+	s.initialize()
+	var data = make(map[string]interface{})
+	data["username"] = "username"
+	data["password"] = "password"
+	sessionToken, _ := s.createUser(data)
+	if sessionToken == "" {
+		t.Error("Expected sessionToken to be initialized")
+	}
+	if s.sessionToken == "" {
+		t.Error("Expected sessionToken to be initialized")
+	}
+}
+
+func TestRequestPasswordReset(t *testing.T) {
+	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer svr.Close()
+	s := NewSession(nil)
+	s.baseUrl, _ = s.baseUrl.Parse(svr.URL)
+	s.applicationId = "applicationId"
+	s.restApiKey = "restApiKey"
+	s.initialize()
+	err := s.passwordReset("email")
+	if err != nil {
+		t.Error("Expected password reset to be successful")
+	}
+}
